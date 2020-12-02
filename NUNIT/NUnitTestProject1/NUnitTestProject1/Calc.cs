36bab28350b2ada5446df6752ec81f21455ca8ad
Roman <rom85382658@gmail.com>
2020-12-02 21:02:03 +0300

    homework

---
+++ NUNIT/NUnitTestProject1/NUnitTestProject1/Calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NUnitTestProject1
+{
+    public static class Calc
+    {
+        static public int Add(int operand1, int operand2)
+        {
+            return operand1 + operand2;
+        }
+
+        static public int Subtraction(int operand1, int operand2)
+        {
+            return operand1 - operand2;
+        }
+
+        static public int Multiply(int operand1, int operand2)
+        {
+            return operand1 * operand2;
+        }
+
+        static public int Partition(int operand1, int operand2)
+        {
+            if (operand2 == 0)
+                return 0;
+            return operand1 / operand2;
+        }
+    }
+}
